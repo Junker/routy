67c953c237739c775d264bdbe1041ce015ca4964
Junker <dk-junker@ya.ru>
2019-03-17 16:23:14 +0700

    info.rkt add "web-server-lib" dependency

--- info.rkt
+++ info.rkt
@@ -2,7 +2,7 @@
 
 (define collection "routy")
 (define version "0.0.1")
-(define deps '("base" "rackunit-lib"))
+(define deps '("base" "web-server-lib" "rackunit-lib"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/routy.scrbl" ())))
 (define pkg-authors '(junker))
