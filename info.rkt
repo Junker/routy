ec117bd966c36758e117466da4fc8bf9ad7a4028
Junker <dk-junker@ya.ru>
2019-03-17 20:34:49 +0700

    version up

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 
 (define collection "routy")
-(define version "0.0.1")
+(define version "0.0.2")
 (define deps '("base" "web-server-lib" "rackunit-lib"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/routy.scrbl" ())))
