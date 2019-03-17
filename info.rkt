158c32ddb90bd6cfd72da496a235f34d55261c22
Junker <dk-junker@ya.ru>
2019-03-18 01:42:08 +0700

    info.rkt fix deps

--- info.rkt
+++ info.rkt
@@ -2,7 +2,7 @@
 
 (define collection "routy")
 (define version "0.1.0")
-(define deps '("base" "web-server-lib" "rackunit-lib" "response-ext"))
+(define deps '("base" "web-server-lib" "rackunit-lib" "racket-route-match" "response-ext"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/routy.scrbl" ())))
 (define pkg-authors '(junker))
